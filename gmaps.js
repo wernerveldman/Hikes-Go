d3ebec37e67ab1674d3df627e923ca8e4b3195ec
Werner <wernerveldman@me.com>
2016-11-29 15:36:38 +0100

    Update gmaps.js

--- gmaps.js
+++ gmaps.js
@@ -18,24 +18,7 @@ $( document ).ready(function() {
                
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
-         navigator.geolocation.watchPosition(geoupdate(data));
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-       }});  
-      });
-      
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
-
-function geoupdate(data){
-         navigator.geolocation.getCurrentPosition(function(position) {
+                  navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {lat: position.coords.latitude,lng: position.coords.longitude};      
             var pos2 = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);          
             currLocation.setPosition(pos);
@@ -81,4 +64,19 @@ function geoupdate(data){
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
-}
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+       }});  
+      });
+      
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+
+
