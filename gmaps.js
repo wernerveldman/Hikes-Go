82da74cff516c7b7086d6241da5994fce19c4c87
Werner <wernerveldman@me.com>
2016-12-15 11:02:16 +0100

    Update gmaps.js

--- gmaps.js
+++ gmaps.js
@@ -80,14 +80,15 @@ $( document ).ready(function() {
                                                }
                                                if (afstand <=500){
                                                $(".coobox").html(next);
-                                                var datum = new Date();
+                                               var datum = new Date();
+                                               console.log(datum - scherm_vertr);
                                                if ((datum - scherm_vertr) >= 2000){        
                                                scherm_vertr = new Date();
                                                $("#overlay").show();
                                                $("#overlay").html("<span class='close'>sluiten</span><p>" + text + "</p>");        
                                                }
                                                }
-                                                                                    }        
+                                               }        
                                     });
                         
                           
